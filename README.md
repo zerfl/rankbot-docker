f56b615c6accd1bf6aa21c9723316c50fa81c237
Daniel Martin <dm4460@gmail.com>
2021-12-11 17:14:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# twitch rank bot
+
+create `.env` file with the following content:
+
+```
+TWITCH_BOT_USERNAME="botname"
+TWITCH_OAUTH_TOKEN="<your oauth token>"
+TWITCH_REFRESH_TOKEN="<your refresh token>"
+TWITCH_CHANNELS=<channel(s) for the bot to join. use a comma separated list for multiple channels> 
+EPIC_USERNAME=<epic username>
+```
+
+run `npm i` to install dependencies
+
+run `npm start` to boot up
---
+++ server.js
@@ -0,0 +1,97 @@
+const fetch = require("node-fetch");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const profileUrlPrefix = `https://api.tracker.gg/api/v2/rocket-league/standard/profile/`;
+const validPlatforms = ["epic", "steam", "xbl", "psn", "switch"];
+
+class HTTPResponseError extends Error {
+  constructor(response, ...args) {
+    super(
+      `HTTP Error Response: ${response.status} ${response.statusText}`,
+      ...args
+    );
+    this.response = response;
+  }
+}
+
+const isValidPlatform = (platform) => {
+  return validPlatforms.includes(platform);
+};
+
+const checkStatus = (response) => {
+  if (response.ok) {
+    return response;
+  } else {
+    throw new HTTPResponseError(response);
+  }
+};
+
+const parseRanks = (data) => {
+  const playlistMapper = new Map();
+  playlistMapper.set(10, "1v1");
+  playlistMapper.set(11, "2v2");
+  playlistMapper.set(13, "3v3");
+
+  const playlists = data.data.segments.filter((segment) => {
+    return (
+      [...playlistMapper.keys()].indexOf(segment.attributes.playlistId) !== -1
+    );
+  });
+  let rank = [];
+
+  playlists.forEach((playlist) => {
+    const playlistName = playlistMapper.get(playlist.attributes.playlistId);
+    const division = playlist.stats.division.metadata.name.replace(
+      "Division",
+      "Div"
+    );
+    const deltaUp = playlist.stats.division.metadata.deltaUp;
+    const deltaDown = playlist.stats.division.metadata.deltaDown;
+
+    if (deltaUp && deltaDown) {
+      rank.push(
+        `${playlistName}: ${playlist.stats.tier.metadata.name} ${division} (${playlist.stats.rating.value} +${deltaUp} -${deltaDown})`
+      );
+    } else {
+      rank.push(
+        `${playlistName}: ${playlist.stats.tier.metadata.name} ${division} (${playlist.stats.rating.value})`
+      );
+    }
+  });
+
+  return rank.join(" // ");
+};
+
+app.get("/:platform/:username", async (req, res) => {
+  const platform = req.params.platform;
+  const username = req.params.username;
+  const url = `${profileUrlPrefix}${platform}/${username}`;
+
+  if (!isValidPlatform(platform)) {
+    res.status(400).send("[rankbot] Invalid platform BrokeBack");
+    return;
+  }
+
+  const response = await fetch(url, {
+    headers: {
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36",
+    },
+  });
+
+  try {
+    checkStatus(response);
+    const data = await response.json();
+    res.send(parseRanks(data));
+  } catch (error) {
+    res
+      .status(error.response.status)
+      .send(`[rankbot] ${error.response.statusText} BrokeBack`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Rocket League Ranks listening at http://localhost:${port}`);
+});
